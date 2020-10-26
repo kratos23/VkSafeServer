29e144f72d79230ce04e3cb6241472d91fd3779a
kaktus23 <nnnnn94@inbox.ru>
2020-10-26 21:12:56 +0300

    Implemented markets servlet

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,12 @@
 import db.data.createStubMarkets
 import db.tables.MarketTable
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.ServletContextHandler
+import org.eclipse.jetty.servlet.ServletHolder
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import servlets.MarketsServlet
 
 fun main() {
     Database.connect("jdbc:sqlite:./db", driver = "org.sqlite.JDBC", user = "root", password = "")
@@ -10,4 +14,10 @@ fun main() {
         SchemaUtils.create(MarketTable)
         createStubMarkets()
     }
+
+    val context = ServletContextHandler(ServletContextHandler.NO_SESSIONS or ServletContextHandler.GZIP)
+    context.addServlet(ServletHolder(MarketsServlet()), "/markets")
+    val server = Server(8080)
+    server.handler = context
+    server.start()
 }
---
+++ src/main/kotlin/servlets/MarketsServlet.kt
@@ -0,0 +1,57 @@
+package servlets
+
+import com.vk.api.sdk.client.VkApiClient
+import com.vk.api.sdk.client.actors.UserActor
+import com.vk.api.sdk.httpclient.HttpTransportClient
+import com.vk.api.sdk.queries.groups.GroupField
+import db.tables.MarketTable
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import vk.VkTokenChecker
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+class MarketsServlet : HttpServlet() {
+
+    override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+        val token = req.getParameter("token")
+        val tokenChecker = VkTokenChecker()
+        val userId = tokenChecker.check(token)
+        if (userId == null) {
+            resp.sendError(401)
+        } else {
+            val q = req.getParameter("q") ?: ""
+            val page = req.parameterMap.getOrDefault("page", arrayOf("0"))[0].toIntOrNull() ?: 0
+            val offset = page.toLong() * PAGE_SIZE.toLong()
+
+            val marketsIds = transaction {
+                MarketTable
+                        .select { MarketTable.name.like("%$q%") }
+                        .orderBy(MarketTable.id)
+                        .limit(PAGE_SIZE, offset)
+                        .map { it[MarketTable.id].value.toString() }
+            }
+            if (marketsIds.isEmpty()) {
+                resp.writer.println("""{"response":[]} """)
+            }
+
+            val userActor = UserActor(userId.toInt(), token)
+            val transportClient = HttpTransportClient.getInstance()
+            val vk = VkApiClient(transportClient)
+            val response = vk.groups().getById(userActor)
+                    .groupIds(marketsIds)
+                    .unsafeParam("language", 0)
+                    .fields(listOf(GroupField.ACTIVITY))
+                    .executeAsString()
+            resp.status = 200
+            resp.contentType = "application/json; charset=UTF-8"
+            resp.writer.println(response)
+        }
+    }
+
+    companion object {
+        const val PAGE_SIZE = 5
+    }
+}
