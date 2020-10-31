a376af495f54ebfa57f4c83ff6d4412c5fcfe2f5
kaktus23 <nnnnn94@inbox.ru>
2020-10-31 16:09:12 +0300

    Store selection implemented

--- src/main/kotlin/servlets/UserMarketServlet.kt
+++ src/main/kotlin/servlets/UserMarketServlet.kt
@@ -5,6 +5,7 @@ import com.vk.api.sdk.client.actors.UserActor
 import com.vk.api.sdk.httpclient.HttpTransportClient
 import com.vk.api.sdk.objects.groups.Fields
 import com.vk.api.sdk.objects.groups.Filter
+import db.entities.Market
 import db.entities.getUserMarket
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.json.JSONObject
@@ -42,5 +43,31 @@ class UserMarketServlet : HttpServlet() {
         }
         resp.contentType = "application/json; charset=UTF-8"
         resp.writer.println(resultJSON.toString())
+    }
+
+    override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
+        val token = req.getParameter("token")
+        val tokenChecker = VkTokenChecker()
+        val userId = tokenChecker.check(token)?.toLongOrNull()
+        if (userId == null) {
+            resp.sendError(401)
+            return
+        }
+        val groupId = req.reader.readText().toLongOrNull()!!
+        val userActor = UserActor(userId.toInt(), token)
+        val transportClient = HttpTransportClient.getInstance()
+        val vk = VkApiClient(transportClient)
+        val group = vk.groups()
+                .getById(userActor)
+                .groupId(groupId.toString())
+                .execute().first()
+        transaction {
+            val market = Market.findById(groupId) ?: Market.new(groupId) {
+                adminId = userId
+                name = group.name
+            }
+            market.adminId = userId
+        }
+        resp.status = 200
     }
 }
